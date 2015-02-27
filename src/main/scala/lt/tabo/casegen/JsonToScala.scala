885a9f31d6615923236cab0728a42a97e54adf1b
Ian Tabolt <ian@tabo.lt>
2015-02-26 21:40:21 -0500

    Add basic implementation
    although it doesn't quite work for multiple case classes, but basic
    types (Int, String, ...) are covered

--- src/main/scala/lt/tabo/casegen/JsonToScala.scala
+++ src/main/scala/lt/tabo/casegen/JsonToScala.scala
@@ -2,19 +2,47 @@ package lt.tabo.casegen
 
 import java.io.InputStreamReader
 
-import org.json4s.JsonAST.JObject
+import org.json4s.JString
+import org.json4s.JsonAST._
 import org.json4s.native.JsonParser
 import treehugger.forest._
 import definitions._
 import treehuggerDSL._
 
+import scala.util.matching.Regex
+
 object JsonToScala {
-  def generateClassFromJson(json: JObject, className: String): Tree = {
-    val TopCaseClass = RootClass.newClass(className)
 
-    val values: Iterable[Tree] = Seq() // TODO: Define inner values of case class
+  def classFor(value: JValue, paramName: String, parent: ClassSymbol=RootClass): Type = value match {
+    case s: JString => StringClass
+    case i: JInt => IntClass
+    case d: JDouble => DoubleClass
+    case o: JObject => generateClassFromJObject(o, toUpperCamel(paramName), parent).tpe
+    case a: JArray => ArrayClass TYPE_OF classFor(a.arr.head, paramName, parent)
+    // TODO: generateClassFromJArray(a, toUpperCamel(paramName)).tpe
+    case x => throw new Error("Don't know how to handle " + x)
+  }
 
-    CASECLASSDEF(TopCaseClass) := BLOCK(values)
+  def generateClassFromJArray(json: JArray, className: String): Tree = {
+    ???
+  }
+
+  def toUpperCamel(str: String) = {
+    val WordStart = "(^|[-_ \\.]+)(\\w)".r
+    WordStart.replaceAllIn(str, _ match {
+      case Regex.Groups(wordBreak, firstChar) =>
+        firstChar.toUpperCase // discard break and upper first char
+    })
+  }
+
+  def generateClassFromJObject(json: JObject, className: String, parent: ClassSymbol=RootClass): Tree = {
+    val TopCaseClass = parent.newClass(className)
+
+    val params: Iterable[ValDef] = for ((name, value) <- json.obj.toIterable) yield {
+      PARAM(name, classFor(value, name, TopCaseClass)).empty
+    }
+
+    CASECLASSDEF(TopCaseClass).withParams(params)
   }
 
   def demo() = {
@@ -24,7 +52,8 @@ object JsonToScala {
       case x => throw new Error("Expected JObject, got " + x)
     }
 
-    val genClass = generateClassFromJson(jObject, "YouTubeResponse")
+    val genClass = generateClassFromJObject(jObject, "YouTubeResponse")
+    println(genClass)
     println(treeToString(genClass))
   }
 
