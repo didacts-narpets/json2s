92b186cbd5c50fe1b8e1f26bfe011ada4edd0c2d
Ian Tabolt <ian.tabolt@mlb.com>
2016-11-30 15:28:23 -0500

    Generate spray jsonFormats as well

--- public/javascripts/scalaform.js
+++ public/javascripts/scalaform.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     $scala.html(prettyPrintOne($scala.html()));
     $scala.addClass("prettyprinted");
     $scala.addClass("prettyprint");
-    $scala.find(".typ").each(function() {
+    $scala.find(".pln, .typ").each(function() {
         $(this).data("curr", $(this).text());
     });
-    $scala.find(".classname .typ").each(function() {
+    $scala.find(".classname .pln, .classname .typ").each(function() {
         var $classname = $(this);
         $classname.editable({
             success: function(response, newValue) {
                 console.log(newValue);
                 var prevValue = $classname.data('curr');
-                $scala.find(".typ").each(function() {
+                $scala.find(".pln, .typ").each(function() {
                     if ($(this).data("curr") == prevValue) {
                         $(this).data("curr", newValue);
                         $(this).text(newValue);
